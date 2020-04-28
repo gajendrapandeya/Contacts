cb7bbecb58d75b3943658b282cb3b773adde63e1
Gajendra Pandeya <gajendrapandeya6@gmail.com>
2020-04-28 15:46:15 +0545

    Contact Objects and ListItem Layout

---
+++ app/src/main/java/com/codermonkeys/contacts/utils/UniversalImageLoader.java
@@ -0,0 +1,43 @@
+package com.codermonkeys.contacts.utils;
+
+import android.content.Context;
+import android.provider.ContactsContract;
+
+import com.codermonkeys.contacts.R;
+import com.nostra13.universalimageloader.cache.memory.impl.WeakMemoryCache;
+import com.nostra13.universalimageloader.core.DisplayImageOptions;
+import com.nostra13.universalimageloader.core.ImageLoaderConfiguration;
+import com.nostra13.universalimageloader.core.assist.ImageScaleType;
+import com.nostra13.universalimageloader.core.display.FadeInBitmapDisplayer;
+
+public class UniversalImageLoader {
+
+    public static final int defaultImage = R.drawable.ic_launcher_background;
+
+    private Context mContext;
+
+    public UniversalImageLoader(Context mContext) {
+        this.mContext = mContext;
+    }
+
+    public ImageLoaderConfiguration getConfig() {
+
+        //Universal Image Loader setup
+        DisplayImageOptions defaultOptions = new DisplayImageOptions.Builder()
+                .showImageOnLoading(defaultImage)
+                .showImageForEmptyUri(defaultImage)
+                .showImageOnFail(defaultImage)
+                .cacheOnDisk(true).cacheInMemory(true)
+                .cacheOnDisk(true).resetViewBeforeLoading(true)
+                .imageScaleType(ImageScaleType.EXACTLY)
+                .displayer(new FadeInBitmapDisplayer(300)).build();
+
+        ImageLoaderConfiguration config = new ImageLoaderConfiguration.Builder(mContext)
+                .defaultDisplayImageOptions(defaultOptions)
+                .memoryCache(new WeakMemoryCache())
+                .diskCacheSize(100 * 1024 * 1024)
+                .build();
+
+        return config;
+    }
+}
