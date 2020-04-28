8fdc2a8f2098a3e0fccb39a52f47a84adc1049a1
Gajendra Pandeya <gajendrapandeya6@gmail.com>
2020-04-28 09:18:10 +0545

    View Contact Fragment Setup

--- app/src/main/java/com/codermonkeys/contacts/MainActivity.java
+++ app/src/main/java/com/codermonkeys/contacts/MainActivity.java
@@ -2,8 +2,12 @@ package com.codermonkeys.contacts;
 
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentTransaction;
 
 import android.os.Bundle;
+
+import com.codermonkeys.contacts.fragments.ViewContactFragment;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -16,5 +20,18 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
 
+        init();
+    }
+
+    private void init() {
+
+        ViewContactFragment fragment = new ViewContactFragment();
+
+        FragmentTransaction transaction = getSupportFragmentManager().beginTransaction();
+        //replace whatever is in the fragment container view with this fragment
+        //and add the transaction to the back stack so that the user can navigate back
+        transaction.replace(R.id.fragment_container, fragment);
+        transaction.addToBackStack(null);
+        transaction.commit();
     }
 }
